03178ee6fad0af57acab1c36df44625358847031
Yueying Tang <yueying.catherine@gmail.com>
2019-01-23 15:08:56 +0800

    feature: update 404 page message

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,9 @@ import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404 Not found" />
+    <h1>PAGE NOT FOUND</h1>
+    <p>I couldn't find the page you are looking for... the sadness.</p>
   </Layout>
 )
 
