e99bcc9c59850dc864ff4648f24712a988cca5b5
Yueying Tang <yueying.catherine@gmail.com>
2019-02-12 16:12:26 +0800

    fix: remove compile warnings

--- src/components/work.js
+++ src/components/work.js
@@ -4,15 +4,13 @@
  */
 
 import React from 'react'
-import {ScreenClassRender} from 'react-grid-system'
 import styled from 'styled-components'
 
 import Container from '@bootstrap-styled/v4/lib/Container'
 import Row from '@bootstrap-styled/v4/lib/Row'
-import Col from '@bootstrap-styled/v4/lib/Col'
+// import Col from '@bootstrap-styled/v4/lib/Col'
 // import Collapse from '@bootstrap-styled/v4/lib/Collapse';
 
-import Emoji from '../utils/emoji'
 import Title from '../components/title'
 import Project from '../components/project'
 
@@ -46,21 +44,6 @@ class Work extends React.Component {
             <Project title="Project Name" subtitle="Project Subtitle" />
             <Project title="Project Name" subtitle="Project Subtitle" />
         </ProjectsRow>
-        <Row>
-          <Col md="12">
-            <div style={{
-              textAlign: "center"
-            }}>
-              <h1>stay tunned</h1>
-              <p>This project is not finished yet. Feel free to check its repo <Emoji symbol="👇" label="point-down" /> for more details :)</p>
-            </div>
-          </Col>
-        </Row>
-        <ScreenClassRender render={screenClass => (
-            <p style={{ fontSize: ['lg', 'xl'].includes(screenClass) ? '2rem' : '1rem' }} >
-              Screen class: {screenClass}
-            </p>
-        )} />
       </Container>
     )
   }
